958436b44b12419fb8da65a2008433eb17b1bdb4
Socrates Utui <socratesutui@live.com>
2024-08-24 16:51:54 +0200

    settled pages and sidebar nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
@@ -17,6 +18,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div
+            className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+          />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -25,7 +29,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src={'/hero-desktop.png'}
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt='Hero image'
+          />
+          <Image
+            src={'/hero-desktop.png'}
+            width={1000}
+            height={760}
+            className="block md:hidden"
+            alt='Hero image'
+          />
         </div>
       </div>
     </main>
